228a8b6c4b22f1949a868eef9cf9a401a36fa180
Rizwan Mustafa <rizwanmustafa0000@gmail.com>
2021-08-16 14:38:51 +0500

    Created API for registering users
    The created API for registering users will be later equipped with the ability to send emails to the potential user to confirm their account

--- server/Utility.py
+++ server/Utility.py
@@ -1,4 +1,5 @@
 from os import urandom
+import re
 import hashlib
 
 
@@ -14,3 +15,49 @@ def HashPassword(password: str, saltUsed: bytes = None):
     )
 
     return hashedPassword, saltUsed
+
+
+def ValidateUserData(username: str, email: str, password: str):
+    # Presence check for all variables
+    if username == None or username == "":
+        return "Username cannot be empty!"
+    elif email == None or email == "":
+        return "Email cannot be empty"
+    elif password == None or password == "":
+        return "Password cannot be empty!"
+
+    # Validate username
+    for x in username:
+        if not x.isalpha() and not x.isnumeric():
+            return "Username can only be alphanumeric!"
+    if len(username) > 50:  # Length Check
+        return "Username should not be longer than 50 characters!"
+
+    # Validate email
+    if IsValidEmail(email):
+        return "Please enter a valid email!"
+
+    # Validate password. Rules: length >= 8. Contains both numbers and alphabets
+    if len(password) < 8:
+        return "Password must be at least 8 characters long!"
+    containsAlpha = containsNum = False
+    for x in password:
+        if x.isalpha():
+            containsAlpha = True
+        if x.isnumeric():
+            containsNum = True
+
+        if containsAlpha and containsNum:
+            break
+
+    if containsAlpha == False or containsNum == False:
+        return "Password must be alphanumeric"
+
+    return ""
+
+
+def IsValidEmail(email: str) -> bool:
+    if not re.search('^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$', email):
+        return False
+
+    return True
--- server/main.py
+++ server/main.py
@@ -1,12 +1,81 @@
-from flask import Flask
+from Utility import HashPassword, ValidateUserData
+from json import dumps as dumpJSON
+
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
+
+from decouple import config as GetEnvVar
+
+from models import db, User
 
 app = Flask(__name__)
+# Update Flask App config
+app.config.update(
+    # General Settings
+    SECRET_KEY=GetEnvVar('SECRET_KEY'),
+    # CORS Settings
+    CORS_HEADERS='Content-Type',
+    # Database Settings
+    SQLALCHEMY_DATABASE_URI=GetEnvVar('DATABASE_URI'),
+    SQLALCHEMY_TRACK_MODIFICATIONS=False,
+)
+
+cors = CORS(app)
+db.init_app(app)
 
 
-@app.route("/")
-def index():
-    return "<h1 style='color:dodgerblue; font-family:sans-serif;'>Hello World</h1>"
+@app.route("/users/", methods=["POST"])
+@cross_origin()
+def manage_users():
+    if request.method == "POST":
+        # Get user data
+        userData = request.get_json()
+
+        # Check if user data is valid
+        userDataValid = ValidateUserData(
+            userData['username'],
+            userData['email'],
+            userData['password']
+        )
+
+        if userDataValid != "":  # Since the data is not valid, send the appropriate correction measure
+            return dumpJSON(userDataValid)
+
+        # Make sure the username or email does not already exist in the database
+        usernameExists = User.query.filter_by(
+            username=userData['username']).first()
+
+        if usernameExists:
+            return dumpJSON("Username is already taken!")
+
+        emailExists = User.query.filter_by(email=userData['email']).first()
+        if emailExists:
+            return dumpJSON("Email is already taken!")
+
+        # Hash the passoord and get the salt used
+        hashedPassword, salt = HashPassword(userData['password'])
+
+        # Create a user object
+        myUser = User(
+            userData['username'],
+            userData['email'],
+            hashedPassword,
+            salt,
+            False,
+            False
+        )
+
+        # Add the user object to the database and commit the changes
+        db.session.add(myUser)
+        db.session.commit()
+
+        # Later send them an email with a link to verify their account
+
+        return dumpJSON("User created!")
+    pass
 
 
 if __name__ == "__main__":
+    with app.test_request_context():
+        db.create_all()
     app.run(debug=True)
