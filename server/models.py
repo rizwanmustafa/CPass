23eec8fd672cf55732234d84ab3ba5e0a1291c41
Rizwan Mustafa <rizwanmustafa0000@gmail.com>
2021-08-17 12:27:20 +0500

    Created method to get user action table
    The action table will store URLs that were generated for the user for a specific reason e.g a URL may be generated for the user to verify their account

--- server/models.py
+++ server/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db: SQLAlchemy = SQLAlchemy()
 
@@ -20,3 +21,26 @@ class User(db.Model):
         self.salt = salt
         self.emailVerified = emailVerified
         self.twoFactorAuthEnabled = twoFactorAuthEnabled
+
+
+def get_user_action_table(username: str):
+    # This table stores that action URLs for a particular user
+    class ActionTable(db.Model):
+        __tablename__ = f'userAction${username}'
+        __table_args__ = {
+            # This is so that when we get this table again even though it is already defined
+            'extend_existing': True,
+        }
+
+        url = db.Column(db.String(8), nullable=False, primary_key=True)
+        id = db.Column(db.Integer, nullable=False)
+        expiry_date = db.Column(db.DateTime, nullable=False)
+
+        def __init__(self, url: str, id: int, expiry_date: datetime):
+            self.url = url
+            self.id = id
+            self.expiry_date = expiry_date
+
+    # Create the table
+    db.create_all()
+    return ActionTable
