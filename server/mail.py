ae80d8785f03e3c2b8e694f324f5e4053b306794
Rizwan Mustafa <rizwanmustafa0000@gmail.com>
2021-11-30 20:42:02 +0500

    1. Added ability to send html in send_mail 2. Changed argument name in send_verification_mail 3. Created a new method for mailing jwt to user

--- server/mail.py
+++ server/mail.py
@@ -6,10 +6,13 @@ from typing import List
 mail = Mail()
 
 
-def send_mail(subject: str, body: str, recipients: List[str]) -> bool:
+def send_mail(subject: str, body: str, recipients: List[str], html=None) -> bool:
     try:
-        msg = Message(subject, recipients, body,
-                      sender=GetEnvVar('MAIL_USERNAME'))
+        msg = Message(subject,
+                      recipients,
+                      body,
+                      html,
+                      GetEnvVar('MAIL_USERNAME'))
         mail.send(msg)
         return True  # Mail was successfully sent
     except Exception as e:
@@ -18,11 +21,28 @@ def send_mail(subject: str, body: str, recipients: List[str]) -> bool:
         return False  # Mail was not successfully sent
 
 
-def send_verification_email(username: str, url: str, recipient: str):
+def send_verification_email(username: str, url: str, email: str):
     url = GetEnvVar("PUBLIC_WEBSITE_URL") + \
         f"/action?username={username}&url={url}"
     email_body = f"Hi {username},\n\nPlease click the following link to verify your account:\n\n{url}\n\nThanks,\n\nCloud Password Manager"
-    return send_mail("Confirm your account", email_body, [recipient])
+    return send_mail("Confirm your account", email_body, [email])
+
+
+def send_jwt_mail(username: str, email: str, token: str, ip_address: str):
+    return send_mail(
+        subject="Verify Login!",
+        body="",
+        recipients=[email],
+        html=f"""
+        <html><body>
+            <p>Hi {username},</p>
+            <p>Please input the following token to verify your login:</p>
+            <p><b>{token}</b></p>
+            <p>IP Address of login attempt:</p>
+            <p><b>{ip_address}</b></p>
+            <b>If this wasn't you, we recommend you to change your password immediately!</b>
+        </body></html>"""
+    )
 
 
 def send_failed_login_email(username: str, email: str,  ip_address: str):
