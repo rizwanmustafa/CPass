3e9fe42e3ee0b953d4cac1237c24a7cb4603207f
Rizwan Mustafa <rizwanmustafa0000@gmail.com>
2022-03-15 17:47:40 +0500

    Added requests handled API

--- backend/index.ts
+++ backend/index.ts
@@ -1,8 +1,22 @@
 #!/usr/bin/env ts-node
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
+let requestsHandled = 0;
+const startDate = new Date();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(morgan('combined'));
+app.use((_req, _res, next) => {
+  requestsHandled += 1
+  next();
+});
+
+app.get("/", (_req, res) => {
+  res.json({
+    "uptime": (new Date().getTime() - startDate.getTime()) / 1000,
+    "requestsHandled": requestsHandled.toString()
+  });
+});
 
 app.listen(5000, () => console.log("The server has started listening on port 5000"));
