edad18899427d50f113b121472c16f745d2fcb6a
Rizwan Mustafa <rizwanmustafa0000@gmail.com>
2022-03-26 10:58:58 +0500

    Prettying the index.ts file

--- backend/index.ts
+++ backend/index.ts
@@ -8,28 +8,33 @@ const app = express();
 let requestsHandled = 0;
 const startDate = new Date();
 
+// Middlewares
 app.use(morgan('combined'));
+
 app.use(cors());
+
 app.use(express.json());
+
 app.use((_req, _res, next) => {
   requestsHandled += 1
   next();
 });
 
-// Add Routers
+// Add routes
 app.use("/users", UserRouter);
 
 app.get("/", (_req, res) => {
   res.json({
-    "uptime": (new Date().getTime() - startDate.getTime()) / 1000,
+    "uptime": new Date().getTime() - startDate.getTime(),
     "requestsHandled": requestsHandled.toString()
   });
 });
 
+// Dynamic 404 Handling
 app.use("*", (_req, res) => {
-    return res.status(404).json({
-      message: "Could not find the resource you were looking for!"
-    });
+  return res.status(404).json({
+    message: "Could not find the resource you were looking for!"
+  });
 });
 
 app.listen(5000, () => console.log("The server has started listening on port 5000"));
