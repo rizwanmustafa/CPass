0739f323c519d7dc6eadf66ac55635a23efde1c0
Rizwan Mustafa <rizwanmustafa0000@gmail.com>
2022-03-30 12:16:23 +0500

    Created winston logger

---
+++ backend/utils/logger.ts
@@ -0,0 +1,87 @@
+import { createLogger, transports, format, Logger as WinstonLogger } from "winston";
+import chalk from "chalk";
+import { resolve } from "path";
+
+// Environment variables
+const logFolderPath = process.env.LOG_FOLDER_PATH ?? "./logs";
+const maxLogSize = parseInt(process.env.LOG_FILE_MAX_SIZE ?? "10485760");
+
+// Console output colors
+const errorColor = chalk.red.bold;
+const warningColor = chalk.yellow.bold;
+const successColor = chalk.green.bold;
+const infoColor = chalk.white;
+
+const customLevels = {
+  error: 0,
+  warning: 1,
+  info: 2,
+  success: 3,
+};
+
+const timestampFormat = format.timestamp({
+  format: "DD-MMM-YYYY HH:mm:ss.SSS",
+});
+
+const simpleOutputFormat = format.printf((log) => {
+  return `${log.timestamp}\t${log.level}: ${log.message}`;
+});
+
+const coloredOutputFormat = format.printf((log) => {
+  let color = infoColor;
+
+  switch (log.level) {
+    case "error":
+      color = errorColor;
+      break;
+    case "warning":
+      color = warningColor;
+      break;
+    case "success":
+      color = successColor;
+      break;
+  }
+
+  return `${log.timestamp}\t${color(log.message)}`;
+});
+
+const fileFormat = format.combine(timestampFormat, simpleOutputFormat);
+
+const consoleFormat = format.combine(timestampFormat, coloredOutputFormat);
+
+const logger = createLogger({
+  levels: customLevels,
+  transports: [
+    new transports.File({
+      level: "error",
+      filename: resolve(logFolderPath, "error.log"),
+      maxsize: maxLogSize,
+      format: fileFormat,
+    }),
+    new transports.File({
+      level: "success",
+      filename: resolve(logFolderPath, "combined.log"),
+      maxsize: maxLogSize,
+      format: fileFormat,
+    }),
+    new transports.Console({
+      level: "success",
+      format: consoleFormat,
+    }),
+  ],
+  exceptionHandlers: [
+    new transports.File({
+      filename: resolve(logFolderPath, "exceptions.log"),
+      format: fileFormat,
+    }),
+  ],
+});
+
+const Logger = {
+  error: (message: string): WinstonLogger => logger.error(message),
+  warning: (message: string): WinstonLogger => logger.warning(message),
+  info: (message: string): WinstonLogger => logger.info(message),
+  success: (message: string): WinstonLogger => logger.log("success", message),
+}
+
+export default Logger;
